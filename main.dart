b51686c963998a06cf38060ba9a59f5659c8a6e0
Muhammad Bilal Ahmad <111138242+bilal20code@users.noreply.github.com>
2023-08-10 23:42:07 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,15 @@
+void main(){
+  var email = "abc@gmail.com";
+  var password = 12345;
+  password = 12345;
+  bool islogin = false;
+  while(islogin == false){
+   if(email == "abc@gmail.com" && password == 12345){
+     print("success full");
+     islogin = true;
+   }
+  else{
+     print("Faiel");
+  }
+}
+  }
